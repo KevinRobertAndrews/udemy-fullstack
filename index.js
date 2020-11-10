18f5dca8936b56c6144d61ee6368c9c94ee4aa9a
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2020-11-09 19:14:53 -0500

    Setup Passport.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({
-    hi: 'there'
-  })
-})
+passport.use(new GoogleStratgey());
 
 const PORT = process.env.PORT || 5000;
 
